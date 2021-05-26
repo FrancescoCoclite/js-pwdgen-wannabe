428a96b06ba94ca5688ad959aec8376a9d489c5b
Francesco <francescococlite@gmail.com>
2021-05-26 14:41:15 +0200

    fine esercizio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var nome = prompt('Inserisci Nome');
+console.log(nome);
+
+var cognome = prompt('Inserisci Cognome');
+console.log(cognome);
+
+
+var colore = prompt('Inserisci colore preferito');
+console.log(colore);
+
+var somma = nome + cognome + colore + '21';
+document.getElementById('testo').innerHTML = somma;
+console.log(somma);
